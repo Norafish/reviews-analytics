c3073dd8b6f2c4f3a86648cc846667aeff52bcb5
Nora Lin <c616252002@yahoo.com.tw>
2021-06-21 19:21:08 +0800

    calc avg len

--- read.py
+++ read.py
@@ -6,3 +6,8 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	print(sum_len)
+print('檔案讀取完了，留言平均長度為', sum_len / len(data))
